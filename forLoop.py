603c24f5411248b0282d0e60d89129bf115c5e08
Christopher Watson <christopher_watson@icloud.com>
2018-09-13 16:36:18 -0400

    init

---
+++ forLoop.py
@@ -0,0 +1,42 @@
+#
+# forLoop.py
+
+
+
+# Create a for loop that prints numbers 1 through 5 (incluing 5!!)
+
+
+
+
+# LINE FOR READABILITY
+print("\n--------------------------------------\n");
+
+
+# Create a for loop that prints numbers 10 through 100 (including 100!!)
+
+
+
+
+
+# LINE FOR READABILITY
+print("\n--------------------------------------\n");
+
+
+
+
+
+# Create a for loop that prints numbers 25 to 1 backwards (including 25 and 1)
+
+
+
+
+
+# LINE FOR READABILITY
+print("\n--------------------------------------\n");
+
+
+
+
+
+# Create a custom for loop !!
+
---
+++ guessNum.py
@@ -0,0 +1,5 @@
+#
+# guessNum.py
+
+# Create your own custom guessing game using for loops and while loops
+# Be creative!
